c6425b0cfe6750489a41278f709497e3e9706d2d
kangyongbong <rkdqhd11@hanmail.net>
2021-10-10 23:47:54 +0900

    feat : 상수 클래스 생성

---
+++ src/main/java/racinggame/constant/ErrorMessageConstant.java
@@ -0,0 +1,7 @@
+package racinggame.constant;
+
+public class ErrorMessageConstant {
+    public static final String ERR_MSG_EMPTY = "[ERROR] 자동차 이름은 공백을 사용할 수 없습니다.";
+    public static final String ERR_MSG_SIZE = "[ERROR] 자동차 이름의 길이는 5자 이하만 가능합니다.";
+    public static final String ERR_MSG_LIST = "[ERROR] 자동차 이름을 입력해주세요.";
+}
---
+++ src/main/java/racinggame/constant/InputDefinitionConstant.java
@@ -0,0 +1,6 @@
+package racinggame.constant;
+
+public class InputDefinitionConstant {
+    public static final String NUM_REG = "^[1-9]*$";
+    public static final short CAR_MAX_LENGTH = 5;
+}
