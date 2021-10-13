f16e0965eba9009cf95c053126562060c5e36773
kangyongbong <rkdqhd11@hanmail.net>
2021-10-13 21:32:49 +0900

    refactor : 메세지 상수 수정

--- src/main/java/racinggame/constant/ErrorMessageConstant.java
+++ src/main/java/racinggame/constant/ErrorMessageConstant.java
@@ -3,5 +3,5 @@ package racinggame.constant;
 public class ErrorMessageConstant {
     public static final String ERR_MSG_EMPTY = "[ERROR] 자동차 이름은 공백을 사용할 수 없습니다.";
     public static final String ERR_MSG_SIZE = "[ERROR] 자동차 이름의 길이는 5자 이하만 가능합니다.";
-    public static final String ERR_MSG_LIST = "[ERROR] 자동차 이름을 입력해주세요.";
+    public static final String ERR_MSG_LOOP = "[ERROR] 정상적으로 진행되지 않았습니다. 다시 시도해주세요.";
 }
--- src/main/java/racinggame/constant/GameMessageConstant.java
+++ src/main/java/racinggame/constant/GameMessageConstant.java
@@ -2,8 +2,8 @@ package racinggame.constant;
 
 public class GameMessageConstant {
     public static final String CAR_LINE_MSG = "-";
-    public static final String INPUT_NAME_MSG = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기주으로 구분)";
+    public static final String INPUT_NAME_MSG = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)";
     public static final String INPUT_LOOP_MSG = "시도할 회수는 몇회인가요?";
-    public static final String OUPUT_RACING_MSG = "%s : %s \n";
-    public static final String OUTPUT_END_MSG = "최종우승자는 %s 입니다. \n";
+    public static final String OUPUT_RACING_MSG = "%s : %s\n";
+    public static final String OUTPUT_END_MSG = "\n최종 우승자는 %s 입니다.\n";
 }
