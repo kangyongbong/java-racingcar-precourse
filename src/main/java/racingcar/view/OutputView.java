f1dc48eb00d10862d3121517c008beacc47ffb5e
kangyongbong <rkdqhd11@hanmail.net>
2022-04-21 18:41:56 +0900

    refactor : modify print format

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,25 +6,31 @@ import racingcar.model.vo.Car;
 public class OutputView {
 
     public void outputResult() {
-        System.out.print(GameMessage.OUTPUT_RESULT.getMessage());
+        System.out.println(GameMessage.OUTPUT_RESULT.getMessage());
     }
 
     public void outputResultCar(Car car) {
-        System.out.printf(
-                GameMessage.OUTPUT_RESULT_CAR
-                        .getMessage()
-                ,car.getCarName()
-                        .getName()
-                ,car.getCarPosition()
-                        .isPosition()
+        System.out.println(String.format(
+                        GameMessage.OUTPUT_RESULT_CAR
+                                .getMessage()
+                        , car.getCarName()
+                                .getName()
+                        , car.getCarPosition()
+                                .isPosition()
+                )
         );
     }
 
     public void outputEndGame(String winner) {
-        System.out.printf(
-                GameMessage.OUTPUT_WINNER
-                        .getMessage()
-                , winner
+        System.out.println(String.format(
+                        GameMessage.OUTPUT_WINNER
+                                .getMessage()
+                        , winner
+                )
         );
+    }
+
+    public void outputError(Exception e) {
+        System.out.println(e.getMessage());
     }
 }
