c163380eeb9eae407e69b58002bb8d12202782d5
kangyongbong <rkdqhd11@hanmail.net>
2022-04-21 17:00:54 +0900

    refator : view output message

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,30 @@
 package racingcar.view;
 
+import racingcar.constant.GameMessage;
+import racingcar.model.vo.Car;
+
 public class OutputView {
+
+    public void outputResult() {
+        System.out.print(GameMessage.OUTPUT_RESULT.getMessage());
+    }
+
+    public void outputResultCar(Car car) {
+        System.out.printf(
+                GameMessage.OUTPUT_RESULT_CAR
+                        .getMessage()
+                ,car.getCarName()
+                        .getName()
+                ,car.getCarPosition()
+                        .isPosition()
+        );
+    }
+
+    public void outputEndGame(String winner) {
+        System.out.printf(
+                GameMessage.OUTPUT_WINNER
+                        .getMessage()
+                , winner
+        );
+    }
 }
